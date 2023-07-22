d5ba633c08a59de37e8b23a822ce7c3d0a5c574b
cjsolis <cjelizondo12@gmail.com>
2023-07-22 12:36:20 -0500

    added project data and created grid for work portfolio

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,10 +1,21 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import { Grid, Paper, Typography } from "@mui/material";
+import projectData from "../data/ProjectData";
 
 export default function Work() {
   return (
-    <div>
-      <Button variant="contained">Work</Button>
-    </div>
+    <>
+      <Grid container spacing={8}>
+        {projectData.map((item) => (
+          <Grid item xs={12} sm={6} key={item.id}>
+            <Paper sx={{ padding: 2 }}>
+              <img src={item.img} alt={item.alt} />
+              <Typography variant="h6">{item.title}</Typography>
+              <Typography variant="body1">{item.description}</Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
