80a173b2816cc64c787439660e817ed972597ddf
cjsolis <cjelizondo12@gmail.com>
2023-07-22 11:53:09 -0500

    switched images and grid in about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,38 +1,40 @@
 import * as React from "react";
 import { Grid, Paper, Typography } from "@mui/material";
-import image1 from "../assets/me7.jpg"
-import image2 from "../assets/me2.jpg"
+import image1 from "../assets/me3.jpg";
+import image2 from "../assets/me2.jpg";
 
 export default function About() {
   return (
-    <Grid
-      container
-      spacing={2}
-    >
+    <Grid container spacing={8}>
       {/* first row */}
       <Grid item xs={12} sm={6}>
-        <Paper style={{ height: "100vh"}}>
-        <img src={image1} alt="personal photo" style={{ width:"100%", height:"100%", objectFit: "contain"}}></img>
-      </Paper>
+      <img
+          src={image2}
+          alt="personal photo"
+          style={{ width: "100%", height: "80%", objectFit: "contain" }}
+        ></img>
       </Grid>
       <Grid item xs={12} sm={6}>
-        <Paper style={{ height: "100vh", background: "" }}>
-          <Typography variant="h6">I made the decision to pursue a career in
-            tech after trying the beauty industry as an esthetician and the education field as a teacher.</Typography>
-        </Paper>
+        <Typography variant="h6">
+          I made the decision to pursue a career in tech after trying the beauty
+          industry as an esthetician and the education field as a teacher.
+        </Typography>
       </Grid>
       {/* second row */}
       <Grid item xs={12} sm={6}>
         {/* Text on the right */}
-        <Paper style={{ height: "100vh", background: "" }}>
+        <Paper style={{ height: "20vh", background: "" }}>
           {/* Add text component here */}
           <Typography variant="h6">Text here</Typography>
         </Paper>
       </Grid>
       <Grid item xs={12} sm={6}>
-        <Paper style={{ height: "100vh"}}>
-        <img src={image2} alt="personal photo" style={{ width:"100%", height:"100%", objectFit: "contain"}}></img>
-      </Paper>
+    
+         <img
+          src={image1}
+          alt="personal photo"
+          style={{ width: "100%", height: "80%", objectFit: "contain" }}
+        ></img>
       </Grid>
     </Grid>
   );
