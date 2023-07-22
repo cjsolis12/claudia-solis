cfed2dbbfd5c7fa9c958383e53a3ae852f65fa87
cjsolis <cjelizondo12@gmail.com>
2023-07-22 12:41:06 -0500

    changed grid sizing

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -7,7 +7,7 @@ export default function Work() {
     <>
       <Grid container spacing={4}>
         {projectData.map((item) => (
-          <Grid item xs={12} sm={6} key={item.id}>
+          <Grid item xs={12} sm={6} key={item.id} style={{ height: "50%", height: "50%"}}>
               <img src={item.img} alt={item.alt} style={{ width: "100%", height: "100%", objectFit: "contain" }} />  
           </Grid>
         ))}
