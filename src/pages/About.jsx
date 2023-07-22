78cbcd91f0f7550733e96ead79856bfbfaa367c8
cjsolis <cjelizondo12@gmail.com>
2023-07-22 11:37:58 -0500

    added a grid to about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,39 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Unstable_Grid2'; 
+import * as React from "react";
+import { Grid, Paper, Typography } from "@mui/material";
+import image1 from "../assets/me7.jpg"
+import image2 from "../assets/me2.jpg"
 
 export default function About() {
   return (
-    
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-      <Button variant="contained">About</Button>
+    <Grid
+      container
+      spacing={2}
+    >
+      {/* first row */}
+      <Grid item xs={12} sm={6}>
+        <Paper style={{ height: "100vh"}}>
+        <img src={image1} alt="personal photo" style={{ width:"100%", height:"100%", objectFit: "contain"}}></img>
+      </Paper>
       </Grid>
-    
+      <Grid item xs={12} sm={6}>
+        <Paper style={{ height: "100vh", background: "" }}>
+          <Typography variant="h6">I made the decision to pursue a career in
+            tech after trying the beauty industry as an esthetician and the education field as a teacher.</Typography>
+        </Paper>
+      </Grid>
+      {/* second row */}
+      <Grid item xs={12} sm={6}>
+        {/* Text on the right */}
+        <Paper style={{ height: "100vh", background: "" }}>
+          {/* Add text component here */}
+          <Typography variant="h6">Text here</Typography>
+        </Paper>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Paper style={{ height: "100vh"}}>
+        <img src={image2} alt="personal photo" style={{ width:"100%", height:"100%", objectFit: "contain"}}></img>
+      </Paper>
+      </Grid>
+    </Grid>
   );
 }
