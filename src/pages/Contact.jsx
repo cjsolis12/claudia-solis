7ebea86f408c48435ca5b695464ade4e5fa40c2c
cjsolis <cjelizondo12@gmail.com>
2023-07-24 13:03:41 -0500

    styled contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,71 +3,69 @@ import {
   Paper,
   TextField,
   Box,
-  TextareaAutosize,
   Button,
-  Grid,
+
 } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
-import contactForm from "../assets/contactForm.jpg"
+import contactForm from "../assets/contactForm.jpg";
 
 export default function Contact() {
   return (
-    <Grid container spacing={4}>
-      <Grid item xs={12} md={4}>
-        <img src={contactForm} alt="contact" style={{ width: "100%", height: "100%", objectFit: "cover"  }} />
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Paper
-          elevation={3}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Box
-            component="form"
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              margin: "15px",
-              padding: "20px",
-              "& .MuiTextField-root": { m: 1, width: "25ch" },
-            }}
-            noValidate
-            autoComplete="off"
-            variant="outlined"
-          >
-            <TextField
-              type="text"
-              name="name"
-              label="Name"
-              variant="standard"
-              required
-            />
-            <TextField
-              type="email"
-              name=""
-              label="Email Address"
-              variant="standard"
-              required
-            />
+    <Paper
+      elevation={3}
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 700
+      }}
+    >
+      <img
+        src={contactForm}
+        alt="contact"
+        style={{ width: "300px", height: "200px", objectFit: "cover" }}
+      />
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: "15px",
+          padding: "20px",
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+        variant="outlined"
+      >
+        <TextField
+          type="text"
+          name="name"
+          label="Name"
+          variant="standard"
+          required
+        />
+        <TextField
+          type="email"
+          name=""
+          label="Email Address"
+          variant="standard"
+          required
+        />
 
-            <TextField
-              name="message"
-              type="text"
-              rows={10}
-              label="Message"
-              style={{ margin: "5px" }}
-            ></TextField>
-            <Button variant="contained" endIcon={<SendIcon />}>
-              Send
-            </Button>
-          </Box>
-        </Paper>
-      </Grid>
-    </Grid>
+        <TextField
+          name="message"
+          type="text"
+          rows={10}
+          label="Message"
+          style={{ margin: "5px" }}
+        ></TextField>
+        <Button variant="contained" endIcon={<SendIcon />}>
+          Send
+        </Button>
+      </Box>
+    </Paper>
   );
 }
